d36c569ba00cd6d4b8221f458a91df789db6d4cf
Ankit Kumar <ankit.kumar@91mobiles.com>
2024-01-06 23:30:57 +0530

    input validator added

---
+++ middlewares/validators/notesValidator.js
@@ -0,0 +1,41 @@
+import {check, validationResult} from 'express-validator';
+
+export const validateNotes = [
+  check('id')
+    .trim()
+    .escape()
+    .not()
+    .isEmpty()
+    .withMessage('Notes ID can not be empty!')
+    .bail()
+    .isNumeric()
+    .withMessage('Notes ID Should be Numeric')
+    .bail(),
+  (req, res, next) => {
+    const errors = validationResult(req);
+    console.log(errors);
+    if (!errors.isEmpty())
+      return res.status(422).json({success:false, errors: errors.array()});
+    next();
+  },
+];
+
+export const validateCreateNotes = [
+    check('note')
+    .trim()
+    .escape()
+    .not()
+    .isEmpty()
+    .withMessage('Note Cannot be Empty!')
+    .bail()
+    .isLength({min: 1})
+    .withMessage('Note cannot be Empty!')
+    .bail(),
+  (req, res, next) => {
+    const errors = validationResult(req);
+    console.log(errors);
+    if (!errors.isEmpty())
+      return res.status(422).json({success:false, errors: errors.array()});
+    next();
+  },
+];
--- routes/notes.js
+++ routes/notes.js
@@ -1,11 +1,9 @@
 import express from "express";
-import {getNotes, getNoteById, createNote} from '../controllers/notesController.js'
+import {getNotes, getNoteById, createNote, updateNote, deleteNote} from '../controllers/notesController.js'
+import {validateNotes, validateCreateNotes} from '../middlewares/validators/notesValidator.js';
 const notesRouter = express.Router();
 
-notesRouter.route('/').get(getNotes).post(createNote);
-
-notesRouter.get('/:id', async (req, res) => {
-    return await getNoteById(req, res);
-})
+notesRouter.route('/').get(getNotes).post(validateCreateNotes, createNote);
+notesRouter.route('/:id').get(validateNotes, getNoteById).patch(validateNotes, updateNote).delete(validateNotes, deleteNote);
 
 export default notesRouter
