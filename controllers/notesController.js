ec329ecce52615bd647efe7a4bc0546302ceb15c
Ankit Kumar <ankit.kumar@91mobiles.com>
2024-01-07 00:42:47 +0530

    next error fix and file refactor

--- controllers/notesController.js
+++ controllers/notesController.js
@@ -1,30 +1,28 @@
-import {addNote, getAllNotes, getNote} from "../database.js"
+import {addNote, getAllNotes, getNote} from "../db/database.js"
 
 export const getNotes = async (req, res, next) => {
     try {
         const notes = await getAllNotes();
-        if( !notes ) return res.json({success: false, notes: {}})
+        // if( !notes ) return res.json({success: false, notes: {}})
         return res.json({success: true, notes})
     }catch( err ) {
         next(err)
     }
 }
 
-export const getNoteById = async (req, res) => {
+export const getNoteById = async (req, res, next) => {
     try {
         const {id, name} = req.params
         const notes = await getNote(id);
-        if( !notes ) return res.json({success: false, notes: {}})
         return res.json({success: true, notes})
     }catch( err ) {
         next(err)
     }
 }
 
-export const createNote = async (req, res) => {
+export const createNote = async (req, res, next) => {
     try {
         const {note} = req.body;
-        if( !note ) return res.status(400).json({success: false, notes: {}})
         const noteRow = await addNote(note);
         if( !noteRow ) return res.status(400).json({success: false, notes: {}})
         
