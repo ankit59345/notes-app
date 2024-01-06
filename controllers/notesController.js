e285856892da08b40f635a845dd2b3df97190fc6
Ankit Kumar <ankit.kumar@91mobiles.com>
2024-01-07 01:11:17 +0530

    async wrapper added

--- controllers/notesController.js
+++ controllers/notesController.js
@@ -1,64 +1,44 @@
 import {addNote, getAllNotes, getNote, updateNote, deleteNote} from "../db/database.js"
 
 export const getNotes = async (req, res, next) => {
-    try {
-        const notes = await getAllNotes();
-        // if( !notes ) return res.json({success: false, notes: {}})
-        return res.json({success: true, notes})
-    }catch( err ) {
-        next(err)
-    }
+    const notes = await getAllNotes();
+    // if( !notes ) return res.json({success: false, notes: {}})
+    return res.json({success: true, notes})
 }
 
 export const getNoteById = async (req, res, next) => {
-    try {
-        const {id, name} = req.params
-        const notes = await getNote(id);
-        return res.json({success: true, notes})
-    }catch( err ) {
-        next(err)
-    }
+    const {id, name} = req.params
+    const notes = await getNote(id);
+    return res.json({success: true, notes})
 }
 
 export const createNote = async (req, res, next) => {
-    try {
-        const {note} = req.body;
-        const noteRow = await addNote(note);
-        if( !noteRow ) return res.status(400).json({success: false, notes: {}})
-        
-        return res.status(201).json({success: true, notes: noteRow})
-    }catch( err ) {
-        next(err)
-    }
+    const {note} = req.body;
+    const noteRow = await addNote(note);
+    if( !noteRow ) return res.status(400).json({success: false, notes: {}})
+    
+    return res.status(201).json({success: true, notes: noteRow})
 }
 
 export const updateExistingNote = async (req, res, next) => {
-    try {
-        const {id} = req.params;
-        const {note} = req.body;
-        console.log(id, note);
-        const noteRow = await getNote(id);
-        if( !noteRow ) return res.status(400).json({success: false, msg: "Data Does not exist", notes: {}})
-        const affectedRows = await updateNote(id, note);
-        if( !affectedRows ) return res.status(400).json({success: false, notes: {}})
-        
-        return res.status(201).json({success: true, notes: affectedRows})
-    }catch( err ) {
-        next(err)
-    }
+    const {id} = req.params;
+    const {note} = req.body;
+    console.log(id, note);
+    const noteRow = await getNote(id);
+    if( !noteRow ) return res.status(400).json({success: false, msg: "Data Does not exist", notes: {}})
+    const affectedRows = await updateNote(id, note);
+    if( !affectedRows ) return res.status(400).json({success: false, notes: {}})
+    
+    return res.status(201).json({success: true, notes: affectedRows})
 }
 
 export const deleteExistingNote = async (req, res, next) => {
-    try {
-        const {id} = req.params;
-        const noteRow = await getNote(id);
-        if( !noteRow ) return res.status(400).json({success: false, msg: "Data Does not exist", notes: {}})
+    const {id} = req.params;
+    const noteRow = await getNote(id);
+    if( !noteRow ) return res.status(400).json({success: false, msg: "Data Does not exist", notes: {}})
 
-        const rowsAffected = await deleteNote(id);
-        const resObj = rowsAffected==1 ? {success: true} : {success: false}
-        
-        return res.status((rowsAffected==1 ? 200 : 420)).json(resObj)
-    }catch( err ) {
-        next(err)
-    }
+    const rowsAffected = await deleteNote(id);
+    const resObj = rowsAffected==1 ? {success: true} : {success: false}
+    
+    return res.status((rowsAffected==1 ? 200 : 420)).json(resObj)
 }
--- routes/notes.js
+++ routes/notes.js
@@ -1,9 +1,10 @@
 import express from "express";
+import {asyncWrapper} from '../middlewares/asyncWrapper.js';
 import {getNotes, getNoteById, createNote, updateExistingNote, deleteExistingNote} from '../controllers/notesController.js'
 import {validateNotes, validateCreateNotes} from '../middlewares/validators/notesValidator.js';
 const notesRouter = express.Router();
 
-notesRouter.route('/').get(getNotes).post(validateCreateNotes, createNote);
-notesRouter.route('/:id').get(validateNotes, getNoteById).patch([validateNotes, validateCreateNotes], updateExistingNote).delete(validateNotes, deleteExistingNote);
+notesRouter.route('/').get(asyncWrapper(getNotes)).post(validateCreateNotes, asyncWrapper(createNote));
+notesRouter.route('/:id').get(validateNotes, asyncWrapper(getNoteById)).patch([validateNotes, validateCreateNotes], asyncWrapper(updateExistingNote)).delete(validateNotes, asyncWrapper(deleteExistingNote));
 
 export default notesRouter
