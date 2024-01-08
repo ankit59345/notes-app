8da8e0a572baf8ff321d41a4c9157407cbab8a4b
Ankit Kumar <ankit.kumar@91mobiles.com>
2024-01-08 12:18:26 +0530

    error handling fix

--- src/errors/custom-error.js
+++ src/errors/custom-error.js
@@ -1,6 +1,7 @@
 class CustomApiError extends Error {
     constructor(message) {
         super(message);
+        this.name = "Http Custom Error"
     }
 }
 
--- src/middlewares/error-handler.js
+++ src/middlewares/error-handler.js
@@ -2,7 +2,7 @@ const errorHandlerMiddleware = (err, req, res, next) => {
     let customError = {
         // set default
         statusCode: err.statusCode || 500,
-        msg: err.message || 'Something went wrong try again later',
+        msg: (err.name == "Http Custom Error" ? err.message : 'Something went wrong try again later'),
     };
     console.log(err.stack || err.msg);
     return res.status(customError.statusCode).send({success: false, msg: customError.msg})
