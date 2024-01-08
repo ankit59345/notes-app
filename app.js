ede8c11ef8d1d3e2244247fd59afee36fda5e6b9
Ankit Kumar <ankit.kumar@91mobiles.com>
2024-01-08 21:53:34 +0530

    security packages added

--- app.js
+++ app.js
@@ -1,17 +1,37 @@
 const express = require("express");
+const app = express();
+
+// Routes Import
 const notesRouter = require("./src/routes/notes.js");
 const userRouter = require("./src/routes/user.js");
-const {readFileSync} = require("fs");
+
+// Middleware Import
 const {authenticateUser} = require('./src/middlewares/auth.js')
-const errorHandlerMiddleware = require('./src/middlewares/error-handler.js')
+const errorHandlerMiddleware = require('./src/middlewares/error-handler.js');
+const helmet = require("helmet");
+const xss = require('xss-clean');
+const cors = require('cors');
+const rateLimiter = require('express-rate-limit');
 
-const app = express();
+// Middlewares
 app.use(express.json())
-
 app.use(errorHandlerMiddleware)
+app.use(helmet());
+app.use(xss());
+app.use(cors());
+app.use(rateLimiter({
+    windowMs: 1000 * 60,
+    max: 60,
+    handler: (req, res) => {
+        res.status(429).json({
+          error: 'Too many requests. Please try again later.',
+        });
+    },
+}));
 
+
+// Routes
 app.use('/api/v1/notes', [authenticateUser, notesRouter])
-
 app.use('/api/v1/user', userRouter)
 
 
--- src/middlewares/error-handler.js
+++ src/middlewares/error-handler.js
@@ -4,7 +4,9 @@ const errorHandlerMiddleware = (err, req, res, next) => {
         statusCode: err.statusCode || 500,
         msg: (err.name == "Http Custom Error" ? err.message : 'Something went wrong try again later'),
     };
-    console.log(err.stack || err.msg);
+    if( err.name != "Http Custom Error" ) {
+        console.log(err.stack || err.msg);
+    }
     return res.status(customError.statusCode).send({success: false, msg: customError.msg})
 }
 
